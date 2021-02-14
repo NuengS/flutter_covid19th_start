0d6acbd08ea1c7b7fcb46f3465cb5eb5f4e200d6
NuengS <58159069+NuengS@users.noreply.github.com>
2021-02-14 21:35:25 +0700

    first commit

---
+++ lib/components/tabmenu.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../screens/todaycovid19th.dart';
+import '../screens/chart.dart';
+import '../screens/About.dart';
+//import '../screens/about.dart';
+
+class Covid19TabMenu extends StatelessWidget {
+  final List<Widget> tabs = [
+    Center(
+      child: TodayCovid19Th(),
+    ),
+    Center(
+      child: Container(
+        child: CovidChartTimeline(),
+      ),
+    ),
+    Center(
+      child: Container(
+        child: Covid14(),
+      ),
+    ),
+  ];
+
+  final _currentTabIndex;
+
+  Covid19TabMenu(this._currentTabIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return tabs[_currentTabIndex];
+  }
+}
